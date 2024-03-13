b9bda030effa6c3a33d2ea06f578505c17b0949f
haikaljs <haikaljavascript@gmail.com>
2024-03-13 15:20:37 +0800

    Users CRUD API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,35 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
 
     public function index()
     {
-        //
+        return User::all();
     }
 
     public function store(Request $request)
     {
-        //
+        $user = User::create(
+            $request->only('first_name', 'last_name', 'email')  + ['password' => Hash::make('123')]
+        );
+
+        return response($user, Response::HTTP_CREATED); // 201
     }
 
     
     public function show(string $id)
     {
-        //
+        return User::find($id);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::find($id);
+        $user->update($request->only('first_name', 'last_name', 'email'));
+
+        return response($user, RESPONSE::HTTP_ACCEPTED); // 202
     }
 
   
     public function destroy(string $id)
     {
-        //
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT); //204
     }
 }
